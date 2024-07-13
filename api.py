f269579289c64aab5cc56646bf7f9556459f74a6
Erik Valle <erik.valle@gmail.com>
2024-07-13 23:28:31 +0200

    Add bored ape hodler endpoint
    Add bored ape hodler endpoint

--- api.py
+++ api.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-	return query
+	return 'N/ACC'
 	
 
 	
 @app.route('/poaps', methods=['GET'])
 def get_poaps():
 	WALLET_ADDRESS = request.args.get('wallet')
-	WALLET_ADDRESS =WALLET_ADDRESS.lower()
+	WALLET_ADDRESS = WALLET_ADDRESS.lower()
 	if not WALLET_ADDRESS:
 		return jsonify({'error': 'Wallet address is required'}), 400
 	
@@ -48,38 +48,43 @@ def get_poaps():
 
 
 
-def check_holder(wallet_address, contract_address):
-    params = {
-        "owner": wallet_address,
-        "asset_contract_address": contract_address,
-        "order_direction": "desc",
-        "offset": "0",
-        "limit": "1"
+
+def is_holder(wallet_address, contract_address):
+    url = f"{'https://eth-mainnet.alchemyapi.io/v2/pGYy75CP_bnPTpplzZngCWOJ9C_uDQYz'}"
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "alchemy_getTokenBalances",
+        "params": [wallet_address, [contract_address]],
+        "id": 1
     }
-    
-    response = requests.get("https://api.opensea.io/api/v1/assets", params=params)
+
+    response = requests.post(url, json=payload, headers=headers)
     if response.status_code == 200:
-        assets = response.json().get('assets', [])
-        return len(assets) > 0
-    else:
-        return False
+        result = response.json().get('result', {})
+        token_balances = result.get('tokenBalances', [])
+        for token in token_balances:
+            balance = int(token.get('tokenBalance', '0x0'), 16)
+            if balance > 0:
+                return 1
+    return 0
 
 @app.route('/check_ape', methods=['GET'])
 def check_holder_endpoint():
     wallet_address = request.args.get('wallet')
+    wallet_address = wallet_address.lower()
     if not wallet_address:
         return jsonify({'error': 'Wallet address is required'}), 400
     
     bored_ape_contract = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
-    mutant_ape_contract = ""
+    mutant_ape_contract = "0x60e4d786628fea6478f785a6d7e704777c86a7c6"
 
-    is_bored_ape_holder = check_holder(wallet_address, bored_ape_contract)
-    is_mutant_ape_holder = check_holder(wallet_address, mutant_ape_contract)
+    is_bored_ape_holder = is_holder(wallet_address, bored_ape_contract)
+    is_mutant_ape_holder = is_holder(wallet_address, mutant_ape_contract)
 
     return jsonify({
         'wallet': wallet_address,
-        'is_bored_ape_holder': is_bored_ape_holder,
-        'is_mutant_ape_holder': is_mutant_ape_holder
+        'is_ape_holder': is_bored_ape_holder+is_mutant_ape_holder 
     })	
 	
 if __name__ == "__main__":
